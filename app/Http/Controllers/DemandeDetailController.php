384a491fa59d47f8f502daa922222dd016973a79
unknown <Anonymous.blessingnzemo243@gmail.com>
2024-06-11 17:53:44 +0100

    Création des  migrations de Direction et Services ainsi que leur factory, seeders, Request et Model

---
+++ app/Http/Controllers/DemandeDetailController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DemandeDetail;
+use App\Http\Requests\StoreDemandeDetailRequest;
+use App\Http\Requests\UpdateDemandeDetailRequest;
+
+class DemandeDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDemandeDetailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DemandeDetail $demandeDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DemandeDetail $demandeDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDemandeDetailRequest $request, DemandeDetail $demandeDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DemandeDetail $demandeDetail)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/DirectionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Direction;
+use App\Http\Requests\StoreDirectionRequest;
+use App\Http\Requests\UpdateDirectionRequest;
+
+class DirectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDirectionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Direction $direction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Direction $direction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDirectionRequest $request, Direction $direction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Direction $direction)
+    {
+        //
+    }
+}
