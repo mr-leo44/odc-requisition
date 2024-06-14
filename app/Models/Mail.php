4b53e1e0bc30a85e389d12fb1b890a50a9725a59
mr-leo44 <lionnelkaniki44@gmail.com>
2024-06-14 11:35:37 +0100

    Ajout de la relation one to many dans le model Mail

--- app/Models/Mail.php
+++ app/Models/Mail.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Demande;
+use App\Models\Approbateur;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Mail extends Model
 {
@@ -13,5 +16,15 @@ class Mail extends Model
         'approbateur_id',
         'is_approved',
     ];
+
+    public function demande() : BelongsTo
+    {
+        return $this->belongsTo(Demande::class);
+    }
+
+    public function approbateur() : BelongsTo
+    {
+        return $this->belongsTo(Approbateur::class);
+    }
 
 }
