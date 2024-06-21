e6001ab6a974e1bdd65799bd844b3d199de247c1
unknown <Anonymous.blessingnzemo243@gmail.com>
2024-06-21 15:17:00 +0100

    Mise à jour du design du modal de création de la demande

--- resources/views/components/createDemande.blade.php
+++ resources/views/components/createDemande.blade.php
@@ -33,109 +33,154 @@
                             </ul>
                         </div>
                     @endif
-                    <div class="flex justify-end input-group-append">
+                    {{-- <div class="flex justify-end input-group-append">
                         <x-primary-button class="add-input text-sm">
                             {{ __('Ajouter') }}
                         </x-primary-button>
-                    </div>
+                    </div> --}}
                     <form class="w-full" action="{{ route('demandes.store') }}" method="POST">
                         @csrf
                         <div id="input-container">
-                            <div class="grid gap-3 md:grid-cols-3 mb-3 text-white">
-                                <div>
-                                    Désignation
+                            <div class="grid gap-4 mb-6 md:grid-cols-6">
+                                <div class="col-span-5">
+                                    <div class="grid gap-32 grid-cols-3">
+                                        <div class="col-span-2">
+                                            <x-input-label
+                                                class="flex-start font-bold block mb-2 text-sm text-gray-900 dark:text-white"
+                                                for="designation" :value="__('Designation')" />
+                                        </div>
+                                        <div class="">
+                                            <x-input-label
+                                                class="flex-start font-bold block mb-2 text-sm text-gray-900 dark:text-white"
+                                                for="qte_demandee" :value="__('Quantité')" />
+                                        </div>
+                                    </div>
                                 </div>
                                 <div>
-                                    Quantité
+                                    <button type="button"
+                                        class="add-input  text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-md p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        <svg class="w-5 h-5 text-white dark:text-white" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="M5 12h14m-7 7V5" />
+                                        </svg>
+                                        <span class="sr-only">Icon description</span>
+                                    </button>
                                 </div>
                             </div>
-                            <div class="grid gap-3 mb-6 md:grid-cols-3 input-group">
-                                <div>
-                                    {{-- <x-input-label
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                    for="designation" :value="__('Designation')" /> --}}
-                                    <x-text-input id="designation"
-                                        class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        type="text" name="demandes[0][designation]" :value="old('designation')"
-                                        placeholder="Ex. Rame papier duplicataire" required autofocus
-                                        autocomplete="designation" />
-                                    <x-input-error :messages="$errors->get('designation')" class="mt-2" />
+                            <div class="grid gap-4 mb-6 md:grid-cols-6 input-group">
+                                <div class="col-span-5">
+                                    <div class="flex justify-between gap-3">
+                                        <x-text-input id="designation"
+                                            class="bg-gray-50 w-[80%] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
+                                            type="text" name="demandes[0][designation]" :value="old('designation')"
+                                            placeholder="Ex. Rame papier duplicataire" required autofocus
+                                            autocomplete="designation" />
+                                        <x-input-error :messages="$errors->get('designation')" class="mt-2" />
+                                        <x-text-input id="qte_demandee"
+                                            class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-[20%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
+                                            type="number" name="demandes[0][qte_demandee]" :value="old('qte_demandee')" required
+                                            autofocus autocomplete="qte_demandee" />
+                                        <x-input-error :messages="$errors->get('qte_demandese')" class="mt-2" />
+                                    </div>
                                 </div>
-                                <div>
-                                    {{-- <x-input-label
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                    for="qte_demandee" :value="__('Quantité')" /> --}}
-                                    <x-text-input id="qte_demandee"
-                                        class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        type="number" name="demandes[0][qte_demandee]" :value="old('qte_demandee')" required
-                                        autofocus autocomplete="qte_demandee" />
-                                    <x-input-error :messages="$errors->get('qte_demandee')" class="mt-2" />
+                                <div class="mt-auto">
+                                    <button type="button"
+                                        class="delete-input text-white bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-md p-2.5 text-center inline-flex items-center me-2 dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800">
+                                        <svg class="w-5 h-5 text-white dark:text-white" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="M5 12h14" />
+                                        </svg>
 
-                                </div>
-
-                                <div class="mt-2.5">
-                                    <a href="#"
-                                        class="cancelButton text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Réinitialiser</a>
+                                        <span class="sr-only">Icon description</span>
+                                    </button>
                                 </div>
 
                             </div>
                         </div>
                         <div class="flex justify-start mb-4">
-                            <button type="submit"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <button type="submit" class="block text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-20 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800
+                                ">
                                 Envoyer
                             </button>
                         </div>
                     </form>
                 </div>
             </div>
-
-            <script>
-                $(document).ready(function() {
-                    var i = 0;
-                    $('.add-input').click(function(e) {
-                        e.preventDefault();
-                        i++;
-                        $('#input-container').append(`
-                <div class="grid gap-3 mb-6 md:grid-cols-3 input-group">
-                    <div>
-                        <x-text-input id="designation"
-                            class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            type="text" name="demandes[${i}][designation]" :value="old('designation')"
-                            placeholder="Ex. Rame papier duplicataire" required autofocus
-                            autocomplete="designation" />
-                        <x-input-error :messages="$errors->get('designation')" class="mt-2" />
-                    </div>
-                    <div>
-                        <x-text-input id="qte_demandee"
-                            class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            type="number" name="demandes[${i}][qte_demandee]" :value="old('qte_demandee')" required
-                            autofocus autocomplete="qte_demandee" />
-                        <x-input-error :messages="$errors->get('qte_demandee')" class="mt-2" />
-                    </div>
-
-                    <div class="mt-2.5">
-                           <a href="#" class="delete-input text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Supprimer</a>
-
-                    </div>
-
-                </div>
-            `);
-                    });
-                    // Ajout de la fonction de suppression
-                    $(document).on('click', '.delete-input', function(e) {
-                        e.preventDefault();
-                        const inputGroups = $('.input-group');
-                        if (inputGroups.length === 1) {
-                            $('#designation').val('');
-                            $('#qte_demandee').val('');
-                        } else {
-                            $(this).closest('.input-group').remove();
-                        }
-                    });
-                });
-            </script>
-
         </div>
     </div>
+    <script>
+        $(document).ready(function() {
+            var i = 0;
+            $('.add-input').click(function(e) {
+                e.preventDefault();
+                i++;
+                $('#input-container').append(`
+                    <div class="grid gap-4 mb-6 md:grid-cols-6 input-group">
+                        <div class="col-span-5">
+                            <div class="flex justify-between gap-3">
+                                <x-text-input id="designation"
+                                    class="bg-gray-50 w-[80%] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
+                                    type="text" name="demandes[0][designation]" :value="old('designation')"
+                                    placeholder="Ex. Rame papier duplicataire" required autofocus
+                                    autocomplete="designation" />
+                                <x-input-error :messages="$errors->get('designation')" class="mt-2" />
+                                <x-text-input id="qte_demandee"
+                                    class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-[20%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
+                                    type="number" name="demandes[0][qte_demandee]" :value="old('qte_demandee')" required
+                                    autofocus autocomplete="qte_demandee" />
+                                <x-input-error :messages="$errors->get('qte_demandese')" class="mt-2" />
+                            </div>
+                        </div>
+                            <div class="mt-auto">
+                                <button type="button"
+                                    class="delete-input text-white bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-md p-2.5 text-center inline-flex items-center me-2 dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800">
+                                    <svg class="w-5 h-5 text-white dark:text-white" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="M5 12h14" />
+                                    </svg>
+
+                                    <span class="sr-only">Icon description</span>
+                                </button>
+                            </div>
+
+                        </div>
+                `);
+            });
+            // Ajout de la fonction de suppression
+            $(document).on('click', '.delete-input', function(e) {
+                e.preventDefault();
+                const inputGroups = $('.input-group');
+                if (inputGroups.length === 1) {
+                    $('#designation').val('');
+                    $('#qte_demandee').val('');
+                } else {
+                    $(this).closest('.input-group').remove();
+                }
+            });
+        });
+        // Sélectionnez le bouton de fermeture du modal
+        // Sélectionnez le bouton de fermeture du modal
+        const closeModalButton = document.querySelector('[data-modal-hide="authentication-modal"]');
+
+        // Sélectionnez le formulaire dans le modal
+        const form = document.querySelector('#authentication-modal form');
+
+        // Sélectionnez le conteneur du formulaire
+        const formContainer = document.querySelector('#input-container');
+
+        // Ajoutez un événement de clic sur le bouton de fermeture
+        closeModalButton.addEventListener('click', () => {
+            // Supprimez le formulaire
+            form.reset();
+        });
+    </script>
+
+</div>
+</div>
 </div>
