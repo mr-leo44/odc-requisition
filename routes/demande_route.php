e61771957dffb3e2d856768024274a136d59cf3f
unknown <Anonymous.blessingnzemo243@gmail.com>
2024-06-25 18:47:55 +0100

    Ajout de la route de la vue des demandes validées

--- routes/demande_route.php
+++ routes/demande_route.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\DemandeController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
+    Route::get('demandes/validate', 'App\Http\Controllers\DemandeController@validate')->name('demandes.validate');
     Route::resource('demandes', DemandeController::class);
+
 });
 require __DIR__.'/auth.php';
 
