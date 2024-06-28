9dff1a637096c732ab9626e01e165ce3a4b4e6c0
Anonymous <blessingnzemo243@gmail.com>
2024-06-28 18:25:21 +0100

    Affichage des demandes dont le user est mangager

--- routes/demande_route.php
+++ routes/demande_route.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::get('demandes/historique', 'App\Http\Controllers\DemandeController@historique')->name('demandes.historique');
-
+    Route::get('demandes/manager', [DemandeController::class, 'demandesManager'])->name('demandes.manager');
     Route::resource('demandes', DemandeController::class);
 
 });
