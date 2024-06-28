f78e47ab9135e5f7d484d7debdaa1e47778a2aa3
Foster Mbayi <145870975+77foster@users.noreply.github.com>
2024-06-28 16:46:03 +0100

    Ajout de deux modals(valider et rejeter) et un bouton généretPdf pour le pdf au-dessous du tableau

--- resources/views/demandes/show.blade.php
+++ resources/views/demandes/show.blade.php
@@ -80,23 +80,23 @@
         </table>  
 
     </div>
-               {{-- <div style="display: flex; justify-content: flex-end; margin-top: 20%;">
+               <div style="display: flex; justify-content: flex-end; margin-top: 20%;">
         
         <a  onclick="Valider(event);" data-modal-target="valider" data-modal-toggle="valider" 
         type="button" class="text-black bg-black-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-black-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 dark:focus:ring-black-800">valider</a>
         <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"  type="button" class="text-black bg-black-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-black-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 dark:focus:ring-black-800">Rejeter</button>
-        <a href="{{route('index')}}" class="text-black bg-black-400 hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-black-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-black-900">Générer pdf</a>
+        <a href="" class="text-black bg-black-400 hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-black-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-black-900">Générer pdf</a>
 
     </div>
     
 
-<x-modalValider/>
-<x-modalRejeter/> --}}
+<x-valider/>
+<x-rejeter/>
 
 
-{{-- </x-app-layout> --}}
 
-    {{-- </div> --}}
+
+    </div>
 
 
 
