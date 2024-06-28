0d6212c3545d246c7feb03ad07bb29e48cb06f0f
Foster Mbayi <145870975+77foster@users.noreply.github.com>
2024-06-28 14:59:53 +0100

    création du tableau qui permettra au demandeur de voir l'évolution de sa demande

--- resources/views/demandes/show.blade.php
+++ resources/views/demandes/show.blade.php
@@ -44,7 +44,59 @@
                 @endforeach
             </tbody>
         </table>
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th colspan="2" scope ="col"  class="px-6 py-3">Service démandeur</th>
+                    <th scope ="col"  class="px-6 py-3"> Magasin </th>
+                    <th scope ="col"  class="px-6 py-3"> Responsable Moyens Généraux</th>
+                    <th scope ="col"  class="px-6 py-3"> Chef de Département Achat & Logistique</th>
+                </tr>
+
+                <tr>
+                    <td scope ="col" class="px-6 py-3">User</td>
+                    <td scope ="col" class="px-6 py-3">Manager</td>
+                    <td colspan="3" scope ="col" class="px-6 py-3"></td>
+                  
+                </tr>
+            </thead>
+            {{-- <tbody>       
+                <tr>
+                        
+                    <td class="px-6 py-4">
+                        {{ $demande->user->name }} <br/>
+                        {{ $demande->status }} {{$demande->created_at}} 
+                    </td>
+                    @foreach($approbateurs as $item)
+                    <td class="px-6 py-4"> {{$item->name}} <br/>
+                        {{$item->created_at}} <br/>
+                        {{ $demande->status }}
+                    </td>
+                    </td>
+                    @endforeach
+                    
+                </tr>
+            </tbody> --}}
+        </table>  
+
     </div>
+               {{-- <div style="display: flex; justify-content: flex-end; margin-top: 20%;">
+        
+        <a  onclick="Valider(event);" data-modal-target="valider" data-modal-toggle="valider" 
+        type="button" class="text-black bg-black-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-black-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 dark:focus:ring-black-800">valider</a>
+        <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"  type="button" class="text-black bg-black-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-black-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 dark:focus:ring-black-800">Rejeter</button>
+        <a href="{{route('index')}}" class="text-black bg-black-400 hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-black-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-black-900">Générer pdf</a>
+
+    </div>
+    
+
+<x-modalValider/>
+<x-modalRejeter/> --}}
+
+
+{{-- </x-app-layout> --}}
+
+    {{-- </div> --}}
 
 
 
