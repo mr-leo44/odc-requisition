d4be080182ff9f8bae180a6660be5446e662bb3a
Cris_Yongo <mariabolikango@gmail.com>
2024-06-28 14:22:19 +0100

    Enregistrement des informations de l'user lors de sa première connexion

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,96 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form action="{{ route('register') }}" method="POST">
         @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <div class="text-center font-bold">
+            <x-input-label for="type" :value="__('Completez le profil')" />
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div id="step-1">
+            <div class="mt-4 ui-widget" id="direction">
+                <x-input-label for="search_direction" :value="__('Votre direction')" />
+                <x-text-input id="search_direction" class="block mt-1 w-full" type="text" name="direction"
+                    :value="old('direction')" required />
+                <x-input-error :messages="$errors->get('direction')" class="mt-2" />
+            </div>
+            <div class="flex items-center justify-end mt-4">
+                <x-primary-button class="ms-4" type="button" id="byStepTwo">
+                    {{ __('Suivant') }}
+                </x-primary-button>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div id="step-2" class="hidden">
+            <div class="mt-4" id="service">
+                <x-input-label for="search_service" :value="__('Votre service')" />
+                <x-text-input id="search_service" class="block mt-1 w-full" type="text" name="service"
+                    :value="old('service')" required />
+                <x-input-error :messages="$errors->get('service')" class="mt-2" />
+            </div>
+            <div class="flex items-center justify-end mt-4" id="byStepThree">
+                <x-primary-button class="ms-4">
+                    {{ __('Suivant') }}
+                </x-primary-button>
+            </div>
         </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div id="step-3" class="hidden">
+            <div class="mt-4 ui-widget">
+                <x-input-label for="search_manager" :value="__('Votre Manager')" />
+                <x-text-input id="search_manager" class="block mt-1 w-full" type="text" name="manager"
+                    :value="old('manager')" required />
+                <x-input-error :messages="$errors->get('manager')" class="mt-2" />
+            </div>
+            <div class="flex items-center justify-end mt-4">
+                <x-primary-button class="ms-4" type="submit">
+                    {{ __('Valider') }}
+                </x-primary-button>
+            </div>
         </div>
     </form>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script type="text/javascript">
+        $(function() {
+            var users = @json($users);
+            var usersData = []
+            for (var i = 0; i < users.length; i++) {
+                usersData.push(users[i]['name']);
+            }
+
+            $("#search_manager").autocomplete({
+                source: usersData,
+            });
+
+            var directions = @json($directions);
+            var directionsData = []
+
+            for (var i = 0; i < directions.length; i++) {
+                directionsData.push(directions[i]['name']);
+            }
+
+            $("#search_direction").autocomplete({
+                source: directionsData
+            });
+
+            var services = @json($services);
+            var servicesData = []
+
+            for (var i = 0; i < services.length; i++) {
+                servicesData.push(services[i]['name']);
+            }
+
+            $("#search_service").autocomplete({
+                source: servicesData,
+            });
+
+            $("#byStepTwo").on('click', function(event) {
+                event.preventDefault();
+                $("#step-2").removeClass('hidden');
+                $("#step-1").addClass('hidden');
+            })
+            $("#byStepThree").on('click', function(event) {
+                event.preventDefault();
+                $("#step-3").removeClass('hidden');
+                $("#step-2").addClass('hidden');
+            })
+        })
+    </script>
 </x-guest-layout>
