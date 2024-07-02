19a1aab3499a998d1df1cbe461718bebe8861629
mr-leo44 <lionnelkaniki44@gmail.com>
2024-07-02 16:18:25 +0100

    retrait du middleware auth pour faciliter la connexion avec l'api

--- routes/demande_route.php
+++ routes/demande_route.php
@@ -3,12 +3,10 @@
 use App\Http\Controllers\DemandeController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
     Route::get('demandes/historique', 'App\Http\Controllers\DemandeController@historique')->name('demandes.historique');
     Route::get('demandes/manager', [DemandeController::class, 'demandesManager'])->name('demandes.manager');
     Route::resource('demandes', DemandeController::class);
 
-});
 require __DIR__.'/auth.php';
 
 
