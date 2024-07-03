589980d55bfbf06e92e0512aa7af83baeb7a5bb2
Foster Mbayi <145870975+77foster@users.noreply.github.com>
2024-07-03 19:22:21 +0100

    flow de validation

--- app/Http/Controllers/TraitementController.php
+++ app/Http/Controllers/TraitementController.php
@@ -2,65 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Demande;
 use App\Models\Traitement;
+use App\Models\Approbateur;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTraitementRequest;
 use App\Http\Requests\UpdateTraitementRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class TraitementController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    /*
+        * Manage the request's validation flow 
+    */
+    public function validate(Request $request, Demande $demande)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreTraitementRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Traitement $traitement)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Traitement $traitement)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateTraitementRequest $request, Traitement $traitement)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Traitement $traitement)
-    {
-        //
+        $en_cours = Traitement::where('demande_id', $demande->id)
+            ->where('status', 'en cours')
+            ->first();
+        if ($request->status === 'validé') {
+            $prochain_level = (int)($en_cours->level) + 1;
+            $prochain_approb = Approbateur::where('level', $prochain_level)->first();
+            $user_approb = User::where('email', $prochain_approb->email)->first();
+            $success = $en_cours->update([
+                'status' => $request->status
+            ]);
+            if ($success) {
+                Traitement::create([
+                    'level' => $en_cours->level + 1,
+                    'demande_id' => $demande->id,
+                    'demandeur_id' => $en_cours->demandeur_id,
+                    'approbateur_id' => $user_approb->id
+                ]);
+            }
+            return redirect()->back()->with('success','Validation reussie');
+        } elseif ($request->status === 'rejeté') {
+            $en_cours->update([
+                'status'=> $request->status,
+                'observation' => $request->observation
+            ]);
+            return redirect()->back()->with('success','Demande rejetée avec succès');
+        } else {
+            return redirect()->back();
+        }
     }
 }
---
+++ routes/webR.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\DemandeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\pdfController;
+use App\Http\Controllers\TraitementController;
+
+
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/generate-pdf', [pdfController::class, 'generatePDF']);
+Route::get('/generate', [pdfController::class, 'generate']);
+Route::get('/index', [pdfController::class, 'index'])->name('index');
+Route:: post('/{demande}/validate',[TraitementController::class, 'validate'])->name('validate');
+
+
+
+require __DIR__ . '/auth.php';
+
