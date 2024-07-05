70dd394807ac41a2c8429ceac721dbf05f29d9c1
chella <ravchelndombe64@gmail.com>
2024-07-05 12:54:37 +0100

    gestion de l'affichage des bouttons de validation

--- resources/views/demandes/show.blade.php
+++ resources/views/demandes/show.blade.php
@@ -79,7 +79,7 @@
                                 </svg>
                             </td>
                         @elseif($traitement->status == 'rejeté')
-                            <td scope="col" class="px-6 py-4">
+                            <td scope="col" class="px-6 py-4 hover:bg-gray-50 dark:hover:bg-gray-600">
                                 <svg class="w-6 h-6 text-red-600 w-full mx-auto dark:text-red-600" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                                     viewBox="0 0 24 24">
@@ -90,7 +90,7 @@
 
                             </td>
                         @else
-                            <td scope="col" class="px-6 py-4">
+                            <td scope="col" class="px-6 py-4 hover:bg-gray-50 dark:hover:bg-gray-600">
                                 <span class="w-full mx-auto">{{ __(' ') }}</span>
 
                             </td>
@@ -101,17 +101,25 @@
         </table>
     </div>
 
-    <div style="display: flex; justify-content: flex-end; margin-top: 20%;">
-        <button id="accept" onclick="accept(event);" data-modal-target="valider" data-modal-toggle="valider"
-            type="button"
-            class="text-white bg-emerald-700 hover:bg-emerald-800 focus:outline-none focus:ring-4 focus:ring-black-300 font-medium rounded-md text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 dark:focus:ring-black-800">
-            Valider
-        </button>
-        <button id="reject" onclick="reject(event)" data-modal-target="popup-modal" data-modal-toggle="popup-modal"
-            type="button"
-            class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-md text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-            Rejeter
-        </button>
+    <div class="flex justify-end mt-4">
+        @if ($demande->manager->email === session()->get('authUser')->email)
+            <button id="accept" onclick="accept(event);" data-modal-target="valider" data-modal-toggle="valider"
+                type="button"
+                class="text-white bg-emerald-700 hover:bg-emerald-800 focus:outline-none focus:ring-4 focus:ring-black-300 font-medium rounded-md text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-black-600 dark:hover:bg-black-700 dark:focus:ring-black-800">
+                Valider
+            </button>
+        @endif
+        @if (session()->get('authUser')->email !== $demande->user->email ||
+                $en_cours->level === 0 || 
+                $en_cours->approbateur_id === session()->get('authUser')->id ||
+                $en_cours->status !== 'rejeté'
+            )
+            <button id="reject" onclick="reject(event)" data-modal-target="popup-modal"
+                data-modal-toggle="popup-modal" type="button"
+                class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-md text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                Rejeter
+            </button>
+        @endif
         <a href=""
             class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Générer
             pdf
