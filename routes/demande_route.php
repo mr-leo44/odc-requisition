1234d914eb35755190fe47cad4c81463163c7b59
Anonymous <blessingnzemo243@gmail.com>
2024-07-08 17:46:37 +0100

    Suppression de la navigation de la route manager et de sa route

--- routes/demande_route.php
+++ routes/demande_route.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\DemandeController;
 use Illuminate\Support\Facades\Route;
 
     Route::get('demandes/historique', 'App\Http\Controllers\DemandeController@historique')->name('demandes.historique');
-    Route::get('demandes/manager', [DemandeController::class, 'demandesManager'])->name('demandes.manager');
     Route::resource('demandes', DemandeController::class);
 
 require __DIR__.'/auth.php';
