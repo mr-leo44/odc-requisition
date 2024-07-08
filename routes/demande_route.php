e63a298475f6d2e17a4ba3b17d61c7e2c273bf8e
Anonymous <blessingnzemo243@gmail.com>
2024-07-08 17:27:37 +0100

    Revert "Suppression de l'affichage pour manager"
    This reverts commit 7f78b06d51ca5e9f330d2dfe1abf7dfae787de6f.

--- routes/demande_route.php
+++ routes/demande_route.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DemandeController;
 use Illuminate\Support\Facades\Route;
 
     Route::get('demandes/historique', 'App\Http\Controllers\DemandeController@historique')->name('demandes.historique');
+    Route::get('demandes/manager', [DemandeController::class, 'demandesManager'])->name('demandes.manager');
     Route::resource('demandes', DemandeController::class);
 
 require __DIR__.'/auth.php';
