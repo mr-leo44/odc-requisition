cc8b83c1e3b4799a9a34ce97f5673949e63d5e48
mr-leo44 <lionnelkaniki44@gmail.com>
2024-07-09 13:00:16 +0100

    ajout du middleware auth qui bloque l'accès à l'user no connecté

--- routes/web_approbateur.php
+++ routes/web_approbateur.php
@@ -1,6 +1,9 @@
 <?php
 use App\Http\Controllers\approbateurController;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth')->group(function(){
     Route::post('/update-levels', [ApprobateurController::class, 'updateLevels'])->name('update.levels');
     Route::resource('approbateurs', ApprobateurController::class);
+});
 require __DIR__.'/auth.php';
