b137d235e3f355b4e8d9ad43d36d189ed826d6f6
Anonymous <blessingnzemo243@gmail.com>
2024-08-06 17:23:10 +0100

    Mise à jour de la navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,7 @@
                             </path>
                         </svg>
                     </button>
-                    <a @if (Session::get('authUser')->compte->role->value === 'user') href="{{ route('demandes.index') }}" 
+                    <a @if (Session::get('authUser')->compte->role->value === 'user') href="{{ route('demandes.index') }}"
                         @elseif(Session::get('authUser')->compte->role->value === 'livraison')
                         href="{{ route('dashboard') }}"
                         @else
@@ -85,7 +85,8 @@
         aria-label="Sidebar">
         <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
             <ul class="space-y-2 font-medium">
-                @profile('livraison')
+
+                {{-- @profile('livraison')
                     <li>
                         <a href="{{ route('dashboard') }}"
                             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
@@ -100,8 +101,9 @@
                             <span class="ms-3">Tableau de bord</span>
                         </a>
                     </li>
-                @endprofile
-                @profile('user')
+                @endprofile --}}
+
+                @if (Session::get('authUser')->compte->role->value !== 'admin')
                     <li>
                         <div id="accordion-flush" data-accordion="collapse"
                             data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white"
@@ -123,10 +125,17 @@
                             </h2>
                             <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
                                 <div class="p-4 border-b border-gray-200 dark:border-gray-700">
-                                    <p
-                                        class="mb-2 text-gray-800 px-7 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
-                                        <a href="{{ route('demandes.index') }}">En cours</a>
-                                    </p>
+                                    @if (Session::get('authUser')->compte->role->value === 'livraison')
+                                        <p
+                                            class="mb-2 text-gray-800 px-7 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                                            <a href="{{ route('demandes.index') }}">A livrer</a>
+                                        </p>
+                                    @else
+                                        <p
+                                            class="mb-2 text-gray-800 px-7 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                                            <a href="{{ route('demandes.index') }}">En cours</a>
+                                        </p>
+                                    @endif
                                     <p
                                         class="text-gray-800 px-7 mb-2 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
                                         <a href="{{ route('demandes.historique') }}">Historique</a>
@@ -135,7 +144,8 @@
                             </div>
                         </div>
                     </li>
-                @endprofile
+                @endif
+
 
                 @profile('admin')
                     <li>
@@ -150,18 +160,20 @@
                             <span class="ms-3">Approbateurs</span>
                         </a>
                     </li>
-                    {{-- <li>
+                    <li>
                         <a href="{{ route('directions.index') }}"
                             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                <path fill-rule="evenodd" d="M5.535 7.677c.313-.98.687-2.023.926-2.677H17.46c.253.63.646 1.64.977 2.61.166.487.312.953.416 1.347.11.42.148.675.148.779 0 .18-.032.355-.09.515-.06.161-.144.3-.243.412-.1.111-.21.192-.324.245a.809.809 0 0 1-.686 0 1.004 1.004 0 0 1-.324-.245c-.1-.112-.183-.25-.242-.412a1.473 1.473 0 0 1-.091-.515 1 1 0 1 0-2 0 1.4 1.4 0 0 1-.333.927.896.896 0 0 1-.667.323.896.896 0 0 1-.667-.323A1.401 1.401 0 0 1 13 9.736a1 1 0 1 0-2 0 1.4 1.4 0 0 1-.333.927.896.896 0 0 1-.667.323.896.896 0 0 1-.667-.323A1.4 1.4 0 0 1 9 9.74v-.008a1 1 0 0 0-2 .003v.008a1.504 1.504 0 0 1-.18.712 1.22 1.22 0 0 1-.146.209l-.007.007a1.01 1.01 0 0 1-.325.248.82.82 0 0 1-.316.08.973.973 0 0 1-.563-.256 1.224 1.224 0 0 1-.102-.103A1.518 1.518 0 0 1 5 9.724v-.006a2.543 2.543 0 0 1 .029-.207c.024-.132.06-.296.11-.49.098-.385.237-.85.395-1.344ZM4 12.112a3.521 3.521 0 0 1-1-2.376c0-.349.098-.8.202-1.208.112-.441.264-.95.428-1.46.327-1.024.715-2.104.958-2.767A1.985 1.985 0 0 1 6.456 3h11.01c.803 0 1.539.481 1.844 1.243.258.641.67 1.697 1.019 2.72a22.3 22.3 0 0 1 .457 1.487c.114.433.214.903.214 1.286 0 .412-.072.821-.214 1.207A3.288 3.288 0 0 1 20 12.16V19a2 2 0 0 1-2 2h-6a1 1 0 0 1-1-1v-4H8v4a1 1 0 0 1-1 1H6a2 2 0 0 1-2-2v-6.888ZM13 15a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-2Z" clip-rule="evenodd"/>
-                            </svg>                              
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                viewBox="0 0 24 24">
+                                <path fill-rule="evenodd"
+                                    d="M5.535 7.677c.313-.98.687-2.023.926-2.677H17.46c.253.63.646 1.64.977 2.61.166.487.312.953.416 1.347.11.42.148.675.148.779 0 .18-.032.355-.09.515-.06.161-.144.3-.243.412-.1.111-.21.192-.324.245a.809.809 0 0 1-.686 0 1.004 1.004 0 0 1-.324-.245c-.1-.112-.183-.25-.242-.412a1.473 1.473 0 0 1-.091-.515 1 1 0 1 0-2 0 1.4 1.4 0 0 1-.333.927.896.896 0 0 1-.667.323.896.896 0 0 1-.667-.323A1.401 1.401 0 0 1 13 9.736a1 1 0 1 0-2 0 1.4 1.4 0 0 1-.333.927.896.896 0 0 1-.667.323.896.896 0 0 1-.667-.323A1.4 1.4 0 0 1 9 9.74v-.008a1 1 0 0 0-2 .003v.008a1.504 1.504 0 0 1-.18.712 1.22 1.22 0 0 1-.146.209l-.007.007a1.01 1.01 0 0 1-.325.248.82.82 0 0 1-.316.08.973.973 0 0 1-.563-.256 1.224 1.224 0 0 1-.102-.103A1.518 1.518 0 0 1 5 9.724v-.006a2.543 2.543 0 0 1 .029-.207c.024-.132.06-.296.11-.49.098-.385.237-.85.395-1.344ZM4 12.112a3.521 3.521 0 0 1-1-2.376c0-.349.098-.8.202-1.208.112-.441.264-.95.428-1.46.327-1.024.715-2.104.958-2.767A1.985 1.985 0 0 1 6.456 3h11.01c.803 0 1.539.481 1.844 1.243.258.641.67 1.697 1.019 2.72a22.3 22.3 0 0 1 .457 1.487c.114.433.214.903.214 1.286 0 .412-.072.821-.214 1.207A3.288 3.288 0 0 1 20 12.16V19a2 2 0 0 1-2 2h-6a1 1 0 0 1-1-1v-4H8v4a1 1 0 0 1-1 1H6a2 2 0 0 1-2-2v-6.888ZM13 15a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-2Z"
+                                    clip-rule="evenodd" />
+                            </svg>
                             <span class="ms-3">Directions</span>
                         </a>
-                    </li> --}}
+                    </li>
                 @endprofile
-
-
             </ul>
         </div>
     </aside>
