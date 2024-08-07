6092149e665ca75e5e03cbcda3e1a9a8e3c93a78
mr-leo44 <lionnelkaniki44@gmail.com>
2024-08-07 14:34:45 +0100

    Modification de la route vers le profile de l'user

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard')->middleware('auth');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/profile', [ProfileController::class, 'profile'])->name('profile.index')->middleware('auth');
 
 require __DIR__ . '/auth.php';
 require __DIR__ . '/demande_route.php';
