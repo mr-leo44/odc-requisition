14d35315e82f932d81df64d7dae66ba1a01cc9e5
mr-leo44 <lionnelkaniki44@gmail.com>
2024-08-12 16:10:05 +0100

    Affichage des users

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Compte;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Session;
 
 class AdminController extends Controller
@@ -33,33 +31,7 @@ class AdminController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:50',
-            'direction' => ['required', 'string'],
-            'manager' => ['required', 'string'],
-            'service' => ['required', 'string'],
-        ]);
-
-        $user_array = explode(' ', $request->name);
-        $response = Http::get("http://10.143.41.70:8000/promo2/odcapi/?method=getUserByName&name=$user_array[0]");
-        if ($response->successful()) {
-            $userResponse = $response->json();
-            $userData = $userResponse['users'][0];
-        }
-        $Users = new User();
-        $Users->name = $request->name;
-        $Users->save();
-        return redirect()->route('Users.index')->with('success', 'User créée avec succès');
-    }
-
-    public function changeRole(Request $request, User $user)
-    {
-        $user = User::find($request->id);
-        $profile = Compte::where('user_id', $user->id)->first();
-        $profile->update([
-            'role' => $request->role,
-        ]);
-        return back()->with('success', 'Rôle attribué avec succès');
+        //
     }
 
     /**
