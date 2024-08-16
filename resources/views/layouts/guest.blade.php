e7d750abe90919434f6384a6d1c2fc06057eec12
mr-leo44 <lionnelkaniki44@gmail.com>
2024-08-16 14:51:58 +0100

    update des pages d'authentification

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Orange Requisition') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,12 +16,6 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 dark:bg-slate-800 bg-gray-100">
-            <div>
-                <a href="/">
- 
-                </a>
-            </div>
-
             <div class="w-full p-4 ">
                 {{ $slot }}
             </div>
