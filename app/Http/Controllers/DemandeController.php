f33d8ec2a5a58daf706d774f1f5c10329722b955
Anonymous <blessingnzemo243@gmail.com>
2024-08-19 17:16:04 +0100

    Upadate: Actualisation de la page historique

--- app/Http/Controllers/DemandeController.php
+++ app/Http/Controllers/DemandeController.php
@@ -232,30 +232,53 @@ class DemandeController extends Controller
         $demande->delete();
         return redirect()->route('demandes.index')->with('success', 'Suppression éffectuée avec succès');
     }
+
     public function historique()
     {
-        $connected_user = Session::get('authUser')->id;
-
-        $isDemandeur = Demande::whereHas('traitement', function ($query) use ($connected_user) {
-            $query->where('demandeur_id', $connected_user);
-        })->exists();
-
-        if ($isDemandeur) {
+        $connected_user = Session::get('authUser');
+        if ($connected_user->compte->role->value === 'user') {
             $demandes = Demande::whereHas('traitement', function (Builder $query) use ($connected_user) {
-                $query->where('demandeur_id', $connected_user)
-                    ->where('status', '!=', 'en cours');
-            })
-                ->orderBy('created_at', 'desc')
-                ->paginate(15);
-        } else {
-            $demandes = Demande::whereHas('traitement', function ($query) use ($connected_user) {
-                $query->where('approbateur_id', $connected_user)
+                $query->where('approbateur_id', $connected_user->id)
+                    ->orWhere('demandeur_id', $connected_user->id)
                     ->where('status', '!=', 'en cours');
             })
                 ->orderBy('created_at', 'desc')
                 ->paginate(15);
         }
-
+        if ($connected_user->compte->role->value === 'livraison') {
+            $reqs = Demande::all();
+            $all_validated_keys = [];
+            foreach ($reqs as $key => $req) {
+                $last = Traitement::where('demande_id', $req->id)->orderBy('id', 'DESC')->first();
+                if ($last->status === 'validé') {
+                    $all_validated_keys[$key] = $req->id;
+                }
+            }
+            $validated_reqs = Demande::whereIn('id', $all_validated_keys)->get();
+            $reqs_delivered = [];
+            foreach ($validated_reqs as $key => $validated) {
+                $req_details = DemandeDetail::where('demande_id', $validated->id)->get();
+                $delivered = 0;
+                foreach ($req_details as $req_detail) {
+                    $req_count = $req_detail->qte_demandee;
+                    $deliveries = Livraison::where('demande_detail_id', $req_detail->id)->get();
+                    $count = 0;
+                    if ($deliveries->count() > 0) {
+                        foreach ($deliveries as $key => $delivery) {
+                            $count += $delivery->quantite;
+                        }
+                    }
+                    if ($req_count === $count) {
+                        $delivered += 1;
+                    }
+                }
+                if ($delivered === $req_details->count()) {
+                    $reqs_delivered[] = $validated;
+                }
+            }
+            $demandes_array = collect($reqs_delivered);
+            $demandes = Demande::whereIn('id', $demandes_array->pluck('id'))->orderBy('created_at', 'desc')->paginate(10);
+        }
         return view('demandes.historique', compact('demandes'));
     }
 
