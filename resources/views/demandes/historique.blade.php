21ef05e7257565e176fae1292a9fc57378ffeec7
mr-leo44 <lionnelkaniki44@gmail.com>
2024-08-27 15:27:32 +0100

    ajout du statut de la demande dans l'historique

--- resources/views/demandes/historique.blade.php
+++ resources/views/demandes/historique.blade.php
@@ -25,32 +25,67 @@
                         <th scope="col" class="px-6 py-3">N°</th>
                         <th scope="col" class="px-6 py-3">Services</th>
                         <th scope="col" class="px-6 py-3">Users</th>
+                        <th scope="col" class="px-6 py-3">Status</th>
                         <th scope="col" class="px-6 py-3">Creation Date</th>
                         <th scope="col" class="px-6 py-3">Action</th>
                     </tr>
                 </thead>
                 <tbody class="bg-white dark:bg-gray-900">
                     @foreach ($demandes as $demande)
-                    <tr class="border-b hover:bg-gray-50 dark:hover:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                            {{ $demande->numero }}
-                        </th>
-                        <td class="px-6 py-4">
-                            {{ $demande->service }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $demande->user->name }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ ucfirst($demande->created_at->locale('fr')->isoFormat('LLLL')) }}
-                        </td>
-                        <td class="px-6 py-4">
-                            <a href="{{ route('demandes.show', $demande->id) }}"
-                                class="py-1 px-4">
-                                <i class="material-icons-outlined text-black dark:text-white hover:text-orange-500">visibility</i>
-                            </a>
-                        </td>
-                    </tr>
+                        <tr class="border-b hover:bg-gray-50 dark:hover:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                {{ $demande->numero }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $demande->service }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $demande->user->name }}
+                            </td>
+                            <td class="px-6 py-4">
+                                @if ($demande->status === 'Delivered')
+                                    <span
+                                        class="opacity-95 border-green-400 border text-green-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
+                                        <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                            <path
+                                                d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
+                                        </svg>
+                                        {{ $demande->status }}
+                                    </span>
+                                @elseif($demande->status === 'Rejected')
+                                    <span
+                                        class="opacity-95 border-red-400 border text-red-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
+                                        <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                            <path
+                                                d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
+                                        </svg>
+                                        {{ $demande->status }}
+                                    </span>
+                                @else
+                                    <span
+                                        class="opacity-95 border-orange-500 border text-orange-400 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
+                                        <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                            <path
+                                                d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
+                                        </svg>
+                                        {{ $demande->status }}
+                                    </span>
+                                @endif
+                            </td>
+                            {{-- ->locale('fr') --}}
+                            <td class="px-6 py-4">
+                                {{ ucfirst($demande->created_at->isoFormat('LLLL')) }}
+                            </td>
+                            <td class="px-6 py-4">
+                                <a href="{{ route('demandes.show', $demande->id) }}" class="py-1 px-4">
+                                    <i
+                                        class="material-icons-outlined text-black dark:text-white hover:text-orange-500">visibility</i>
+                                </a>
+                            </td>
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
@@ -70,8 +105,8 @@
             language: {
                 searchPlaceholder: "Search...",
                 paginate: {
-                    previous: "Précédent",
-                    next: "Suivant"
+                    previous: "Previous",
+                    next: "Next"
                 }
             },
             lengthChange: false, // Désactive la sélection du nombre d'éléments par page
