b079b68cab79e7d460767cfc2aa72a30e87c5348
mr-leo44 <lionnelkaniki44@gmail.com>
2024-09-03 19:28:26 +0100

    update de l'index de la demande

--- resources/views/demandes/index.blade.php
+++ resources/views/demandes/index.blade.php
@@ -1,132 +1,159 @@
 <x-app-layout>
-    <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded ">
-        <div class="bg-slate-300 dark:bg-gray-800 rounded-t mb-0 px-4 py-3 border-0">
-            <div class="flex flex-wrap items-center">
-                <div class="relative w-full px-4 max-w-full flex-grow flex-1">
-                    <h3 class=" font-bold text-base dark:text-white">Requests In progress</h3>
-                </div>
-                <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-                    @if (Session::get('authUser')->compte->role->value !== 'livraison')
-                    <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" type="button">
-                        <svg class="w-[44px] h-[44px] text-orange-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z" clip-rule="evenodd"/>
-                        </svg>
-                    </button>
-                    @endif
+    <x-slot name="header">
+        <div class="flex flex-col md:flex-row md:items-center md:justify-between">
+            <h2 class="font-semibold text-3xl text-gray-800 mb-3 md:mb-0 dark:text-white leading-tight">
+                {{ __('Demandes de Requisition') }}
+            </h2>
+            <div>
+                <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-styled-tab"
+                    data-tabs-toggle="#default-styled-tab-content"
+                    data-tabs-active-classes="text-white bg-gray-900 px-6 py-3 dark:bg-orange-500 rounded-lg"
+                    data-tabs-inactive-classes="text-gray-500 hover:text-gray-600 dark:text-gray-400 hover:border-gray-300 dark:hover:text-gray-300"
+                    role="tablist">
+                    <li class="me-2" role="presentation">
+                        <button class="inline-block ease-in transition-all duration-75 p-4 rounded-lg"
+                            id="ongoing-styled-tab" data-tabs-target="#styled-ongoing" type="button" role="tab"
+                            aria-controls="ongoing" aria-selected="false">Mes demandes en cours</button>
+                    </li>
+                    <li class="me-2" role="presentation">
+                        <button
+                            class="inline-block ease-in transition-all duration-75 p-4 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                            id="collaborators-styled-tab" data-tabs-target="#styled-collaborators" type="button"
+                            role="tab" aria-controls="collaborators" aria-selected="false">Demandes des
+                            collaborateurs</button>
+                    </li>
+                    <li class="me-2" role="presentation">
+                        <button
+                            class="inline-block ease-in transition-all duration-75 p-4 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                            id="validate-styled-tab" data-tabs-target="#styled-validate" type="button" role="tab"
+                            aria-controls="validate" aria-selected="false">Demandes à valider</button>
+                    </li>
+                    <li class="me-2" role="presentation">
+                        <button
+                            class="inline-block ease-in transition-all duration-75 p-4 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                            id="historics-styled-tab" data-tabs-target="#styled-historics" type="button" role="tab"
+                            aria-controls="historics" aria-selected="false">Historique</button>
+                    </li>
+                    <li class="me-2" role="presentation">
+                        <button
+                            class="inline-block ease-in transition-all duration-75 p-4 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                            id="statistics-styled-tab" data-tabs-target="#styled-statistics" type="button"
+                            role="tab" aria-controls="statistics" aria-selected="false">Statistiques</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </x-slot>
+
+    <div class="px-4 sm:px-6 lg:px-8">
+        <div>
+            <div
+                class="w-full p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                <div class="text-gray-900 dark:text-white">
+                    <div id="default-styled-tab-content">
+                        <x-ongoing-reqs :demandes="$demandes"/>
+                        <x-collaborators />
+                        <x-validate />
+                        <x-historics />
+                        <x-statistics-reqs />
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    @if (session('success'))
-    <div id="alert-3"
-        class="flex items-center p-4 my-4 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
-        role="alert">
-        <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-            viewBox="0 0 20 20">
-            <path
-                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-        </svg>
-        <span class="sr-only">Info</span>
-        <div class="ms-3 text-sm font-medium">
-            {{ session('success') }}
-        </div>
-        <button type="button"
-            class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700"
-            data-dismiss-target="#alert-3" aria-label="Close">
-            <span class="sr-only">Close</span>
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-            </svg>
-        </button>
-    </div>
-    @endif
-
-
-    <head>
-        <!-- DataTables CSS -->
-        <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css">
-        <!-- DataTables JS -->
-        <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
-    </head>
-    <div class="bg-white dark:bg-gray-800 p-4 shadow-md sm:rounded-lg">
-        <div class="overflow-x-auto">
-            <table id="example" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-slate-300 dark:bg-gray-800 dark:text-white">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            N° Requests
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Service
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            User
-
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            ACTIONS
-                        </th>
-                    </tr>
-                </thead>
-                <tbody class="bg-white dark:bg-gray-900">
-                    @foreach ($demandes as $demande)
-
-                    <tr
-                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row"
-                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {{ $demande->numero }}
-                        </th>
-                        <td class="px-6 py-4">
-                            {{ $demande->service }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $demande->user->name }}
-                        </td>
-                        <td class="flex space-x-2">
-                            <a href="{{ route('demandes.show', $demande->id) }}"
-                                class="py-1 px-4">
-                                <i class="material-icons-outlined text-black dark:text-white hover:text-orange-500">visibility</i>
-                            </a>
-                            @if (session()->get('authUser')->id == $demande->user_id && $demande->level === 0)
-                            <a onclick="supprimer(event);" data-modal-target="delete-modal"
-                                data-modal-toggle="delete-modal"
-                                href="{{ route('demandes.destroy', $demande->id) }}">
-                                <svg class="w-[32px] h-[32px] text-red-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
-                                </svg> 
-                            </a>
-                            @else
-                            <a href="#"></a>
-                            @endif
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="mt-4">
-            {{ $demandes->links() }}
-        </div>
-
-        <x-createDemande />
-        <x-deleteDemande />
-    </div>
     <script>
-        new DataTable('#example', {
-            info: false,
-            ordering: true,
-            paging: true,
-            pageLength: {{$demandes-> perPage()}},
-            language: {
-                paginate: {
-                    previous: "Précédent",
-                    next: "Suivant"
-                }
-            },
-            lengthChange: false, // Désactive la sélection du nombre d'éléments par page
+        const ongoingButton = document.getElementById("ongoing-styled-tab");
+        const collaboratorsButton = document.getElementById("collaborators-styled-tab");
+        const validateButton = document.getElementById("validate-styled-tab");
+        const historicsButton = document.getElementById("historics-styled-tab");
+        const statisticsButton = document.getElementById("statistics-styled-tab");
+        const ongoingTab = document.getElementById("styled-ongoing");
+        const collaboratorsTab = document.getElementById("styled-collaborators");
+        const validateTab = document.getElementById("styled-validate");
+        const historicsTab = document.getElementById("styled-historics");
+        const statisticsTab = document.getElementById("styled-statistics");
+
+        ongoingButton.addEventListener("click", function() {
+            localStorage.setItem('viewTab', 'ongoing')
+            toggleTab()
         });
+        collaboratorsButton.addEventListener("click", function() {
+            localStorage.setItem('viewTab', 'collaborators')
+            toggleTab()
+        });
+        validateButton.addEventListener("click", function() {
+            localStorage.setItem('viewTab', 'validate')
+            toggleTab()
+        });
+        historicsButton.addEventListener("click", function() {
+            localStorage.setItem('viewTab', 'historics')
+            toggleTab()
+        });
+        statisticsButton.addEventListener("click", function() {
+            localStorage.setItem('viewTab', 'statistics')
+            toggleTab()
+        });
+
+        function toggleTab() {
+            const viewTab = localStorage.getItem('viewTab')
+            if (viewTab === 'historics') {
+                historicsButton.setAttribute('aria-selected', true)
+                ongoingButton.setAttribute('aria-selected', false)
+                collaboratorsButton.setAttribute('aria-selected', false)
+                validateButton.setAttribute('aria-selected', false)
+                statisticsButton.setAttribute('aria-selected', false)
+                historicsTab.classList.remove('hidden')
+                ongoingTab.classList.add('hidden')
+                collaboratorsTab.classList.add('hidden')
+                validateTab.classList.add('hidden')
+                statisticsTab.classList.add('hidden')
+            } else if (viewTab === 'statistics') {
+                statisticsButton.setAttribute('aria-selected', true)
+                ongoingButton.setAttribute('aria-selected', false)
+                collaboratorsButton.setAttribute('aria-selected', false)
+                validateButton.setAttribute('aria-selected', false)
+                historicsButton.setAttribute('aria-selected', false)
+                statisticsTab.classList.remove('hidden')
+                ongoingTab.classList.add('hidden')
+                collaboratorsTab.classList.add('hidden')
+                validateTab.classList.add('hidden')
+                historicsTab.classList.add('hidden')
+            } else if (viewTab === 'collaborators') {
+                statisticsButton.setAttribute('aria-selected', false)
+                ongoingButton.setAttribute('aria-selected', false)
+                collaboratorsButton.setAttribute('aria-selected', true)
+                validateButton.setAttribute('aria-selected', false)
+                historicsButton.setAttribute('aria-selected', false)
+                statisticsTab.classList.add('hidden')
+                ongoingTab.classList.add('hidden')
+                collaboratorsTab.classList.remove('hidden')
+                validateTab.classList.add('hidden')
+                historicsTab.classList.add('hidden')
+            } else if (viewTab === 'validate') {
+                statisticsButton.setAttribute('aria-selected', false)
+                ongoingButton.setAttribute('aria-selected', false)
+                collaboratorsButton.setAttribute('aria-selected', false)
+                validateButton.setAttribute('aria-selected', true)
+                historicsButton.setAttribute('aria-selected', false)
+                statisticsTab.classList.add('hidden')
+                ongoingTab.classList.add('hidden')
+                collaboratorsTab.classList.add('hidden')
+                validateTab.classList.remove('hidden')
+                historicsTab.classList.add('hidden')
+            } else {
+                ongoingButton.setAttribute('aria-selected', true)
+                historicsButton.setAttribute('aria-selected', false)
+                collaboratorsButton.setAttribute('aria-selected', false)
+                validateButton.setAttribute('aria-selected', false)
+                statisticsButton.setAttribute('aria-selected', false)
+                ongoingTab.classList.remove('hidden')
+                historicsTab.classList.add('hidden')
+                collaboratorsTab.classList.add('hidden')
+                validateTab.classList.add('hidden')
+                statisticsTab.classList.add('hidden')
+            }
+        }
+        toggleTab()
     </script>
 </x-app-layout>
