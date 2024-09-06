291847f736e0a7d00418a44ed09da1cfaee6b448
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-09-06 16:50:34 +0100

    Update : Mise à jour de la page Register

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-orange-500 opacity-85 border border-transparent rounded-md font-semibold text-xs text-white uppercase text-center tracking-widest hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-orange-500 opacity-85 border border-transparent rounded-md font-semibold text-xs text-white uppercase text-center tracking-widest hover:bg-white hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
