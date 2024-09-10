40e0184725358b9048637db9aed52be962bdaec2
mr-leo44 <lionnelkaniki44@gmail.com>
2024-09-10 13:44:17 +0100

    Ajout du component Collaborateurs

---
+++ app/View/Components/Reqs/Collaborators.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\View\Components\Demandes;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class Collaborators extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.reqs.collaborators');
+    }
+}
