53e04340a230b8499164d44134e3b22f3b26a521
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-09-11 13:39:39 +0100

    Mise à jour de la navigation

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -2,8 +2,12 @@
 
 namespace App\View\Components;
 
-use Illuminate\View\Component;
+use App\Models\User;
 use Illuminate\View\View;
+use App\Models\Approbateur;
+use Illuminate\View\Component;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Database\Eloquent\Builder;
 
 class AppLayout extends Component
 {
@@ -12,6 +16,23 @@ class AppLayout extends Component
      */
     public function render(): View
     {
-        return view('layouts.app');
+        $connected_user = Session::get('authUser');
+        $isManager = User::whereHas('compte', function (Builder $query) use ($connected_user) {
+            $query->where('manager', $connected_user->id)->where('user_id', '!=', $connected_user->id);
+        })->exists();
+        $isValidator = Approbateur::where('email', $connected_user->email)->exists();
+        if ($isValidator && $isManager) {
+            $profile = "Manager et Approbateur";
+        } elseif ($isValidator) {
+            $profile = "Approbateur";
+        } elseif ($isManager) {
+            $profile = "Manager";
+        } elseif ($connected_user->compte->role->value === 'livraison') {
+            $profile = "Livreur";
+        } else {
+            $profile = "";
+        }
+
+        return view('layouts.app', compact('profile'));
     }
 }
