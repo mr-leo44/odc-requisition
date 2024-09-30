640faf1124fade4fbf89e6f80585d25eb55f0e53
mr-leo44 <lionnelkaniki44@gmail.com>
2024-09-30 14:38:13 +0100

    drop des components non utilisés

--- resources/views/components/historics.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="hidden p-4 rounded-lg" id="styled-historics" role="tabpanel" aria-labelledby="historics-tab">
-    Historique
-</div>
--- resources/views/components/validate.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="hidden p-4 rounded-lg" id="styled-validate" role="tabpanel" aria-labelledby="validate-tab">
-    Valider
-</div>
