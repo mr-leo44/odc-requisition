6e6fd881db2d93da900ea8efda4fffad2db8df3f
mr-leo44 <lionnelkaniki44@gmail.com>
2024-10-01 15:38:08 +0100

    ajout de la methode only dans la route resource de demande

--- routes/demande_route.php
+++ routes/demande_route.php
@@ -1,14 +1,11 @@
 <?php
 
-use App\Http\Controllers\DemandeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DemandeController;
 
 Route::middleware(['auth', 'role:not-admin,user,livraison'])->group(function() {
     Route::get('demandes/historique', 'App\Http\Controllers\DemandeController@historique')->name('demandes.historique');
-    Route::resource('demandes', DemandeController::class);
+    Route::resource('demandes', DemandeController::class)->only(['index', 'store', 'destroy']);
 });
 
 require __DIR__.'/auth.php';
-
-
-
