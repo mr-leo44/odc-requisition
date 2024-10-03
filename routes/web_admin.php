7bf7cf831038b4f370b840ad99b7a5a174118f3e
mr-leo44 <lionnelkaniki44@gmail.com>
2024-10-03 12:34:38 +0100

    update des fichiers de route

--- routes/web_admin.php
+++ routes/web_admin.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::resource('admin', AdminController::class);
+    Route::resource('admin', AdminController::class)->only(['index', 'store']);
     Route::put('/admin/{user}/change-role', [AdminController::class, 'changeRole'])->name('users.changeRole');
     Route::post('/admin/{user}/activation', [AdminController::class, 'activateAccount'])->name('users.activation');
     Route::get('/admin/{user}/activation', [AdminController::class, 'activateAccount'])->name('users.activation');
